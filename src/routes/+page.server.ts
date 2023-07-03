7e8162a120dac801cab7229d844dfbe1f13c977a
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-07-02 23:33:11 -0500

    Render basic conversions.

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,11 @@
+export const load = async ({ fetch }) => {
+	const response = await fetch(
+		'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd,krw'
+	)
+	const json = await response.json()
+
+	return {
+		satsPerUsd: 100_000_000 / json.bitcoin.usd,
+		satsPerKrw: 100_000_000 / json.bitcoin.krw,
+	}
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
 	import '../app.scss'
 
+	// From load():
+	export let data
+
 	// Bindings:
 	let query = ''
 
 	const parseQuery = (q: string) => {
-		let query = q.toLowerCase() // Normalize query.
+		const query = q.toLowerCase() // Normalize query.
 
 		let value = parseFloat(query.replace(/[^0-9.]/g, ''))
+		if (isNaN(value)) {
+			value = 1
+		}
 
 		let unit = 'none'
 		if (query.includes('$') || query.includes('usd')) {
@@ -32,6 +38,12 @@
 
 	<div><b>value:</b> {parsedQuery.value}</div>
 	<div><b>unit:</b> {parsedQuery.unit}</div>
+
+	<hr />
+
+	<div>BTC&#8680;SAT: {(parsedQuery.value * 100_000_000).toLocaleString()}</div>
+	<div>USD&#8680;SAT: {(parsedQuery.value * data.satsPerUsd).toLocaleString()}</div>
+	<div>KRW&#8680;SAT: {(parsedQuery.value * data.satsPerKrw).toLocaleString()}</div>
 </main>
 
 <style>
