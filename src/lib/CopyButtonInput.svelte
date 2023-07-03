04c0b0908bcdc5458402da3542c4ce7a711062b3
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-07-03 04:57:35 -0500

    Fix broken pico css input group.

--- src/lib/CopyButtonInput.svelte
+++ src/lib/CopyButtonInput.svelte
@@ -22,11 +22,8 @@
 		<iconify-icon icon="clarity:copy-to-clipboard-line" inline />
 		{label}
 	</button>
-
-	<div class="wrapInput">
-		<input type="text" {value} on:focus={handleFocusInput} />
-		<div class="copied" hidden={!copied}>Copied!</div>
-	</div>
+	<div class="copied" hidden={!copied}>Copied!</div>
+	<input type="text" {value} on:focus={handleFocusInput} />
 </div>
 
 <style>
@@ -34,12 +31,13 @@
 		min-width: 10em;
 	}
 
-	.wrapInput {
+	div[role='group'] {
 		position: relative;
 	}
 
 	/* Place inside input right side, vertically centered */
 	.copied {
+		z-index: 1000;
 		position: absolute;
 		top: 50%;
 		right: 0.5em;
