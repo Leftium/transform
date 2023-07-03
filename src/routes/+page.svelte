2dcdadceabbe87b4852db580127caece70f8e103
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-07-02 23:08:45 -0500

    Detect and show value + unit.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,37 @@
 <script lang="ts">
-	import '../app.scss';
+	import '../app.scss'
+
+	// Bindings:
+	let query = ''
+
+	const parseQuery = (q: string) => {
+		let query = q.toLowerCase() // Normalize query.
+
+		let value = parseFloat(query.replace(/[^0-9.]/g, ''))
+
+		let unit = 'none'
+		if (query.includes('$') || query.includes('usd')) {
+			unit = 'usd'
+		} else if (query.includes('btc')) {
+			unit = 'btc'
+		} else if (query.includes('krw')) {
+			unit = 'krw'
+		}
+
+		return {
+			value,
+			unit,
+		}
+	}
+
+	$: parsedQuery = parseQuery(query)
 </script>
 
 <main class="container">
-	<div><input type="text" /></div>
+	<div><input type="text" bind:value={query} /></div>
+
+	<div><b>value:</b> {parsedQuery.value}</div>
+	<div><b>unit:</b> {parsedQuery.unit}</div>
 </main>
 
 <style>
