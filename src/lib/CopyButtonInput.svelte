108d294fb8234e1ec2afc5ff96f86f977d5e0a07
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-07-03 00:46:11 -0500

    Add clipboard copy icon.

--- src/lib/CopyButtonInput.svelte
+++ src/lib/CopyButtonInput.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
+	import 'iconify-icon'
+
 	// Component props:
 	export let label: string = ''
 	export let value: string = ''
 </script>
 
 <div role="group">
-	<button>{label}</button>
+	<button><iconify-icon icon="clarity:copy-to-clipboard-line" inline /> {label}</button>
 	<input type="text" {value} />
 </div>
 
 <style>
 	button {
-		min-width: 9em;
+		min-width: 10em;
 	}
 </style>
