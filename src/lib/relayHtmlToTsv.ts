5e4b71602385834626dc3291027edf553a35a419
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-12-02 04:39:54 +0900

    Use COLUMN_ARRAY instead of _MAP.

--- src/lib/relayHtmlToTsv.ts
+++ src/lib/relayHtmlToTsv.ts
@@ -1,123 +1,146 @@
 import _ from 'lodash'
 import * as cheerio from 'cheerio'
 
-const COLUMN_MAP: Record<string, { institutionIcon: string; column: string; balance: string }> = {
-	'360 Checking ***0247': {
-		column: 'Q',
-		balance: '$1,000.31',
-		institutionIcon: 'Capital One',
-	},
-	'360 Performance Savings ***1671': {
-		column: 'R',
-		balance: '$50,100.00',
-		institutionIcon: 'Capital One',
-	},
-	'Investor Checking ***7195': {
-		column: 'S',
-		balance: '$500.45',
-		institutionIcon: 'Charles Schwab',
-	},
-	'Checking ***1117': {
-		column: 'T',
-		balance: '$1,000.00',
-		institutionIcon: 'Citibank',
-	},
-	'Citi® Accelerate Savings ***6821': {
-		column: 'U',
-		balance: '$11,132.59',
-		institutionIcon: 'Citibank',
-	},
-	PayPal: {
-		column: 'V',
-		balance: '$30.65',
-		institutionIcon: '',
-	},
-	'Checking - 1820': {
-		column: 'W',
-		balance: '$101.12',
-		institutionIcon: '',
-	},
-	'Savings - 0426': {
-		column: 'X',
-		balance: '$1,092.21',
-		institutionIcon: '',
-	},
-	'USD account ***3304': {
-		column: 'P',
-		balance: '$14,034.97',
-		institutionIcon: '',
-	},
-	'Blue Cash Everyday® ***1004': {
-		column: 'AE',
-		balance: '-$29.31',
-		institutionIcon: 'American Express',
-	},
-	'BoA NEA MC': {
-		column: 'AF',
-		balance: '-$188.49',
-		institutionIcon: '',
-	},
-	'Barclays View Mastercard ***2732': {
-		column: 'AG',
-		balance: '$0.00',
-		institutionIcon: '',
-	},
-	'Quicksilver ***1658': {
-		column: 'AH',
-		balance: '$0.00',
-		institutionIcon: 'Capital One',
-	},
-	'REI Co-op Mastercard ***9770': {
-		column: 'AI',
-		balance: '$0.00',
-		institutionIcon: 'Capital One',
-	},
-	'Amazon Prime Rewards Visa Signature Card ***3898': {
-		column: 'AJ',
-		balance: '$0.00',
-		institutionIcon: 'Chase',
-	},
-	'PayPal Mastercard ***4132': {
-		column: 'AK',
-		balance: '-$0.51',
-		institutionIcon: '',
-	},
-	'SoFi Credit Card ***6550': {
-		column: 'AL',
-		balance: '-$67.12',
-		institutionIcon: '',
-	},
-	'WELLS FARGO AUTOGRAPH VISA® CARD ...7484 ***7484': {
-		column: 'AM',
-		balance: '-$1,070.98',
-		institutionIcon: 'Wells Fargo',
-	},
-	'Individual ***1766': {
+const COLUMN_ARRAY = [
+	{
+		title: 'Individual ***1766',
 		column: '',
 		balance: '$0.07',
 		institutionIcon: 'Charles Schwab',
 	},
-	'Roth Contributory IRA ***4487': {
-		column: 'Z',
-		balance: '$71,122.16',
-		institutionIcon: 'Charles Schwab',
-	},
-	'SoFi Active Investing ***8688': {
-		column: 'AB',
-		balance: '$303.72',
+	{
+		title: 'USD account ***3304',
+		column: 'P',
+		balance: '$14,034.97',
 		institutionIcon: '',
 	},
-	"John's Account ***195": {
+	{
+		title: '360 Checking ***0247',
+		column: 'Q',
+		balance: '$1,000.31',
+		institutionIcon: 'Capital One',
+	},
+	{
+		title: '360 Performance Savings ***1671',
+		column: 'R',
+		balance: '$50,100.00',
+		institutionIcon: 'Capital One',
+	},
+	{
+		title: 'Investor Checking ***7195',
+		column: 'S',
+		balance: '$500.45',
+		institutionIcon: 'Charles Schwab',
+	},
+	{
+		title: 'Checking ***1117',
+		column: 'T',
+		balance: '$1,000.00',
+		institutionIcon: 'Citibank',
+	},
+	{
+		title: 'Citi® Accelerate Savings ***6821',
+		column: 'U',
+		balance: '$11,132.59',
+		institutionIcon: 'Citibank',
+	},
+	{
+		title: 'PayPal',
+		column: 'V',
+		balance: '$30.65',
+		institutionIcon: '',
+	},
+	{
+		title: 'Checking - 1820',
+		column: 'W',
+		balance: '$101.12',
+		institutionIcon: '',
+	},
+	{
+		title: 'Savings - 0426',
+		column: 'X',
+		balance: '$1,092.21',
+		institutionIcon: '',
+	},
+	{
+		title: "John's Account ***195",
 		column: 'Y',
 		balance: '$10,880.00',
 		institutionIcon: '',
 	},
-	'ROTH_IRA ***1985': {
+	{
+		title: 'Roth Contributory IRA ***4487',
+		column: 'Z',
+		balance: '$71,122.16',
+		institutionIcon: 'Charles Schwab',
+	},
+	{
+		title: 'ROTH_IRA ***1985',
 		column: 'AA',
 		balance: '$52,020.20',
 		institutionIcon: 'Vanguard',
 	},
-}
+	{
+		title: 'SoFi Active Investing ***8688',
+		column: 'AB',
+		balance: '$303.72',
+		institutionIcon: '',
+	},
+	{
+		title: 'Blue Cash Everyday® ***1004',
+		column: 'AE',
+		balance: '-$29.31',
+		institutionIcon: 'American Express',
+	},
+	{
+		title: 'BoA NEA MC',
+		column: 'AF',
+		balance: '-$188.49',
+		institutionIcon: '',
+	},
+	{
+		title: 'Barclays View Mastercard ***2732',
+		column: 'AG',
+		balance: '$0.00',
+		institutionIcon: '',
+	},
+	{
+		title: 'Quicksilver ***1658',
+		column: 'AH',
+		balance: '$0.00',
+		institutionIcon: 'Capital One',
+	},
+	{
+		title: 'REI Co-op Mastercard ***9770',
+		column: 'AI',
+		balance: '$0.00',
+		institutionIcon: 'Capital One',
+	},
+	{
+		title: 'Amazon Prime Rewards Visa Signature Card ***3898',
+		column: 'AJ',
+		balance: '$0.00',
+		institutionIcon: 'Chase',
+	},
+	{
+		title: 'PayPal Mastercard ***4132',
+		column: 'AK',
+		balance: '-$0.51',
+		institutionIcon: '',
+	},
+	{
+		title: 'SoFi Credit Card ***6550',
+		column: 'AL',
+		balance: '-$67.12',
+		institutionIcon: '',
+	},
+	{
+		title: 'WELLS FARGO AUTOGRAPH VISA® CARD ...7484 ***7484',
+		column: 'AM',
+		balance: '-$1,070.98',
+		institutionIcon: 'Wells Fargo',
+	},
+]
 
 // Excel Column to Index:
 function columnToNumber(s: string) {
@@ -140,7 +163,8 @@ export const relayHtmlToTsv = (html: string) => {
 	const ccAccounts = cc('li:has([data-mjs="accounts-account"])')
 
 	const output: Record<string, string>[] = []
-	const data: Record<string, { institutionIcon: string; balance: string; column: string }> = {}
+	const data: Array<{ title: string; institutionIcon: string; balance: string; column: string }> =
+		[]
 
 	const accounts = _.map(ccAccounts, (account) => {
 		const ccAccount = cc(account)
@@ -158,14 +182,22 @@ export const relayHtmlToTsv = (html: string) => {
 			.replace(/\s+/g, ' ')
 			.trim()
 
-		const column = COLUMN_MAP[title]?.column || ''
+		const column = _.find(COLUMN_ARRAY, { title })?.column || ''
 		const index = columnToNumber(column)
 
-		data[title] = {
+		data.push({
+			title,
 			column,
 			balance,
 			institutionIcon,
-		}
+		})
+
+		data.sort((a, b) => {
+			const aColumnIndex = columnToNumber(a.column)
+			const bColumnIndex = columnToNumber(b.column)
+
+			return aColumnIndex - bColumnIndex
+		})
 
 		const item = {
 			title,
