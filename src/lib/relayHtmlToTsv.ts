7d1048889f89e68c95913b9953b670d4375e8207
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-12-02 05:55:32 +0900

    Auto-fill columns.

--- src/lib/relayHtmlToTsv.ts
+++ src/lib/relayHtmlToTsv.ts
@@ -16,73 +16,73 @@ const COLUMN_ARRAY = [
 	},
 	{
 		title: '360 Checking ***0247',
-		column: 'Q',
+		column: '',
 		balance: '$1,000.31',
 		institutionIcon: 'Capital One',
 	},
 	{
 		title: '360 Performance Savings ***1671',
-		column: 'R',
+		column: '',
 		balance: '$50,100.00',
 		institutionIcon: 'Capital One',
 	},
 	{
 		title: 'Investor Checking ***7195',
-		column: 'S',
+		column: '',
 		balance: '$500.45',
 		institutionIcon: 'Charles Schwab',
 	},
 	{
 		title: 'Checking ***1117',
-		column: 'T',
+		column: '',
 		balance: '$1,000.00',
 		institutionIcon: 'Citibank',
 	},
 	{
 		title: 'Citi® Accelerate Savings ***6821',
-		column: 'U',
+		column: '',
 		balance: '$11,132.59',
 		institutionIcon: 'Citibank',
 	},
 	{
 		title: 'PayPal',
-		column: 'V',
+		column: '',
 		balance: '$30.65',
 		institutionIcon: '',
 	},
 	{
 		title: 'Checking - 1820',
-		column: 'W',
+		column: '',
 		balance: '$101.12',
 		institutionIcon: '',
 	},
 	{
 		title: 'Savings - 0426',
-		column: 'X',
+		column: '',
 		balance: '$1,092.21',
 		institutionIcon: '',
 	},
 	{
 		title: "John's Account ***195",
-		column: 'Y',
+		column: '',
 		balance: '$10,880.00',
 		institutionIcon: '',
 	},
 	{
 		title: 'Roth Contributory IRA ***4487',
-		column: 'Z',
+		column: '',
 		balance: '$71,122.16',
 		institutionIcon: 'Charles Schwab',
 	},
 	{
 		title: 'ROTH_IRA ***1985',
-		column: 'AA',
+		column: '',
 		balance: '$52,020.20',
 		institutionIcon: 'Vanguard',
 	},
 	{
 		title: 'SoFi Active Investing ***8688',
-		column: 'AB',
+		column: '',
 		balance: '$303.72',
 		institutionIcon: '',
 	},
@@ -94,53 +94,72 @@ const COLUMN_ARRAY = [
 	},
 	{
 		title: 'BoA NEA MC',
-		column: 'AF',
+		column: '',
 		balance: '-$188.49',
 		institutionIcon: '',
 	},
 	{
 		title: 'Barclays View Mastercard ***2732',
-		column: 'AG',
+		column: '',
 		balance: '$0.00',
 		institutionIcon: '',
 	},
 	{
 		title: 'Quicksilver ***1658',
-		column: 'AH',
+		column: '',
 		balance: '$0.00',
 		institutionIcon: 'Capital One',
 	},
 	{
 		title: 'REI Co-op Mastercard ***9770',
-		column: 'AI',
+		column: '',
 		balance: '$0.00',
 		institutionIcon: 'Capital One',
 	},
 	{
 		title: 'Amazon Prime Rewards Visa Signature Card ***3898',
-		column: 'AJ',
+		column: '',
 		balance: '$0.00',
 		institutionIcon: 'Chase',
 	},
 	{
 		title: 'PayPal Mastercard ***4132',
-		column: 'AK',
+		column: '',
 		balance: '-$0.51',
 		institutionIcon: '',
 	},
 	{
 		title: 'SoFi Credit Card ***6550',
-		column: 'AL',
+		column: '',
 		balance: '-$67.12',
 		institutionIcon: '',
 	},
 	{
 		title: 'WELLS FARGO AUTOGRAPH VISA® CARD ...7484 ***7484',
-		column: 'AM',
+		column: '',
 		balance: '-$1,070.98',
 		institutionIcon: 'Wells Fargo',
 	},
 ]
+
+const COLUMNS = _.map(COLUMN_ARRAY, (item, i, collection) => {
+	if (i > 0) {
+		const prevItem = collection[i - 1]
+		if (!item.column && prevItem.column) {
+			item.column = numberToColumn(columnToNumber(prevItem.column) + 1)
+		}
+	}
+	return item
+})
+
+function numberToColumn(num: number) {
+	let letters = ''
+	while (num >= 0) {
+		letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[num % 26] + letters
+		num = Math.floor(num / 26) - 1
+	}
+	return letters
+}
 
 // Excel Column to Index:
 function columnToNumber(s: string) {
@@ -182,7 +201,7 @@ export const relayHtmlToTsv = (html: string) => {
 			.replace(/\s+/g, ' ')
 			.trim()
 
-		const column = _.find(COLUMN_ARRAY, { title })?.column || ''
+		const column = _.find(COLUMNS, { title })?.column || ''
 		const index = columnToNumber(column)
 
 		data.push({
@@ -190,13 +209,6 @@ export const relayHtmlToTsv = (html: string) => {
 			column,
 			balance,
 			institutionIcon,
-		})
-
-		data.sort((a, b) => {
-			const aColumnIndex = columnToNumber(a.column)
-			const bColumnIndex = columnToNumber(b.column)
-
-			return aColumnIndex - bColumnIndex
 		})
 
 		const item = {
@@ -210,6 +222,19 @@ export const relayHtmlToTsv = (html: string) => {
 		output[index] = item
 
 		return item
+	})
+
+	data.sort((a, b) => {
+		const aColumnIndex = columnToNumber(a.column)
+		const bColumnIndex = columnToNumber(b.column)
+
+		return aColumnIndex - bColumnIndex
+	})
+
+	_.forEachRight(data, (item, i, collection) => {
+		if (i > 0 && columnToNumber(item.column) - 1 === columnToNumber(collection[i - 1].column)) {
+			item.column = ''
+		}
 	})
 
 	console.table(data)
